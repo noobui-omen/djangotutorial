f815fde9c7a690ec85cee76264876d2e5ae89bc0
Darsh <darshshani7@gmail.com>
2026-03-07 13:33:15 -0500

    Add STATIC_ROOT for EB deployment

--- mysite/settings.py
+++ mysite/settings.py
@@ -61,6 +61,7 @@ TIME_ZONE = "America/New_York"
 USE_I18N = True
 USE_TZ = True
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
