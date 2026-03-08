db2579ae9f21877f598d2c1cdb285223d03dc42e
Darsh <darshshani7@gmail.com>
2026-03-07 21:14:33 -0500

    Fixed EB Health issue

--- mysite/urls.py
+++ mysite/urls.py
@@ -18,8 +18,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 from django.views.generic.base import RedirectView
+from polls import views
 
 urlpatterns = [
+    path("health/", views.health, name="health"),
     path("", RedirectView.as_view(url="/polls/", permanent=False)),
     path("polls/", include("polls.urls")),
     path("admin/", admin.site.urls),
